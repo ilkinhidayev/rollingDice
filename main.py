b155546184b1a56a6527dfe1701221f7bbff95e6
Ilkin <45667243+ilkinhidayev@users.noreply.github.com>
2023-07-09 03:08:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+print("------------- DICE ROLLING SIMULATOR --------------")
+
+again = True
+
+while again:
+    print(random.randint(1, 6))
+    another_roll = input("Want to roll the dice again? (y/n): ")
+    if another_roll.lower() == "y":
+        continue
+    else:
+        break
